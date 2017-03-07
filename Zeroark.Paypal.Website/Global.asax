00ba89e215f442780ab230c8e6d39919e6d9f23d
Chris Leon <zeroark@gmail.com>
2017-03-07 18:14:03 -0500

    Initial project structure
    Added initial project structure. For now will only use a website project.

---
+++ Zeroark.Paypal.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zeroark.Paypal.Website.MvcApplication" Language="C#" %>
---
+++ Zeroark.Paypal.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zeroark.Paypal.Website.Startup))]
+namespace Zeroark.Paypal.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
